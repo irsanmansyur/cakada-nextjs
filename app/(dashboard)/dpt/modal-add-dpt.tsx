d018476cc0c606ffc2efd60b45a7cdd2d661a02e
Irsan Mansyur <irsan00mansyur@gmail.com>
2024-11-01 00:23:27 +0700

    update data pencarian dan add dpt

--- app/(dashboard)/dpt/modal-add-dpt.tsx
+++ app/(dashboard)/dpt/modal-add-dpt.tsx
@@ -1,242 +1,274 @@
+import { InputErrorMessage } from "@/components/form/input-error";
 import { KecamatanSelect2024 } from "@/components/kecamatans/s";
 import { KelurahanSelect2024 } from "@/components/kelurahan/select-kelurahan";
 import { TDpt } from "@/utils/type/dpt";
 import { TKecamatan, TKelurahan } from "@/utils/type/kecamatan";
 import {
-  Description,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
+	Description,
+	Dialog,
+	DialogPanel,
+	DialogTitle,
 } from "@headlessui/react";
 import useAxios from "axios-hooks";
 import { useState } from "react";
+import Swal from "sweetalert2";
+import { z } from "zod";
 
 type DptAddModalProps = {
-  kabKode: number;
-  onSucces?: (dptNew: TDpt) => void;
+	kabKode: number;
+	onSucces?: (dptNew: TDpt) => void;
 };
 export function DptAddModal({
-  kabKode,
-  onSucces = (dptNew: TDpt) => dptNew,
+	kabKode,
+	onSucces = (dptNew: TDpt) => dptNew,
 }: DptAddModalProps) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [data, setData] = useState({
-    jenisKelamin: "",
-    namaTps: "",
-    tempatLahir: "",
-    nama: "",
-    rt: "",
-    rw: "",
-    nik: "",
-  });
-  const [kecamatan, setKecamatan] = useState<TKecamatan | null>(null);
-  const [kelurahan, setKelurahan] = useState<TKelurahan | null>(null);
-  const [{ loading }, postDpt] = useAxios(
-    {
-      url: process.env.NEXT_PUBLIC_DOMAIN + `/api/dpt/${kabKode}`,
-      method: "POST",
-    },
-    { manual: true },
-  );
+	const [isOpen, setIsOpen] = useState(false);
+	const [data, setData] = useState({
+		jenisKelamin: "",
+		namaTps: "",
+		tempatLahir: "",
+		nama: "",
+		rt: "",
+		rw: "",
+		nik: "",
+	});
+	const [kecamatan, setKecamatan] = useState<TKecamatan | null>(null);
+	const [kelurahan, setKelurahan] = useState<TKelurahan | null>(null);
+	const [{ loading }, postDpt] = useAxios(
+		{
+			url: process.env.NEXT_PUBLIC_DOMAIN + `/api/dpt/${kabKode}`,
+			method: "POST",
+		},
+		{ manual: true },
+	);
+	const [errors, setErrors] = useState<Record<string, string>>({});
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (loading || !kecamatan || !kelurahan) return;
-    const dptNew = {
-      ...data,
-      idKec: kecamatan.wilId,
-      idKel: kelurahan.wilId,
-    };
-    postDpt({ data: dptNew })
-      .then((res) => {
-        if (res.status < 300) {
-          onSucces(res.data["data"]);
-          setIsOpen(false);
-        }
-      })
-      .catch(({ response }) => {});
-  };
+	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (loading || !kecamatan || !kelurahan) return;
+		const dptNew = {
+			...data,
+			idKec: kecamatan.wilId,
+			idKel: kelurahan.wilId,
+		};
+		const validate = (d: object) => {
+			const dptAdd = z.object({
+				jenisKelamin: z
+					.string()
+					.min(1, { message: "Jenis Kelamin Harus Di pilih" }),
+			});
 
-  return (
-    <>
-      <button
-        type="button"
-        className="btn btn-outline btn-primary flex whitespace-nowrap"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        + DPT
-      </button>
-      <Dialog
-        open={isOpen}
-        onClose={() => setIsOpen(false)}
-        className="relative z-50"
-      >
-        <div className="fixed inset-0 flex w-screen items-center justify-center p-4 bg-gray-900 bg-opacity-40 backdrop-blur">
-          <DialogPanel className="max-w-xl space-y-4 border rounded bg-white p-4 shadow">
-            <DialogTitle className="font-bold">Tambah DPT</DialogTitle>
-            <Description>
-              Tambahkan data DPT jika tidak ada data yang sesuai.
-            </Description>
-            <form className="space-y-3" onSubmit={onSubmit}>
-              <div className="flex gap-3 flex-col md:flex-row">
-                <div className="w-full">
-                  <label>Pilih Kecamatan</label>
-                  <KecamatanSelect2024
-                    kabId={kabKode}
-                    onChange={(kec) => {
-                      setKecamatan(kec);
-                      setKelurahan(null);
-                    }}
-                    kecamatan={kecamatan}
-                  />
-                </div>
-              </div>
-              <div className="flex gap-3 mt-3 flex-col md:flex-row">
-                <div className="w-full md:w-1/2">
-                  <label>Pilih Kelurahan</label>
-                  <KelurahanSelect2024
-                    kecId={kecamatan?.wilId}
-                    kelurahan={kelurahan}
-                    kabId={kabKode}
-                    onChange={(kel) => {
-                      setKelurahan(kel);
-                    }}
-                  />
-                </div>
-                <div className="w-full md:w-1/2">
-                  <label htmlFor="namaTps">Pilih TPS</label>
-                  <input
-                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="namaTps"
-                    type="text"
-                    onChange={(e) =>
-                      setData({ ...data, namaTps: e.target.value })
-                    }
-                  />
-                </div>
-              </div>
-              <div className="mb-4 md:flex md:justify-between gap-3">
-                <div className="w-full md:w-1/2">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="name"
-                  >
-                    Nama
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="name"
-                    type="text"
-                    placeholder="Nama"
-                    onChange={(e) => setData({ ...data, nama: e.target.value })}
-                  />
-                </div>
-                <div className="w-full md:w-1/2">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="nik"
-                  >
-                    NIK
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="nik"
-                    type="text"
-                    placeholder="73xxxx"
-                    max={16}
-                    onChange={(e) => setData({ ...data, nik: e.target.value })}
-                  />
-                </div>
-              </div>
-              <div className="mb-4 md:flex md:justify-between gap-3">
-                <div className="w-full md:w-1/2">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="rt"
-                  >
-                    RT
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="rt"
-                    type="text"
-                    placeholder="001"
-                    onChange={(e) => setData({ ...data, rt: e.target.value })}
-                  />
-                </div>
-                <div className="w-full md:w-1/2">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="rw"
-                  >
-                    RW
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="rw"
-                    type="text"
-                    placeholder="00"
-                    max={16}
-                    onChange={(e) => setData({ ...data, rw: e.target.value })}
-                  />
-                </div>
-              </div>
+			const { error } = dptAdd.safeParse(d);
+			if (error) {
+				const formattedErrors = error.errors.reduce((acc: any, error: any) => {
+					const [key] = error.path;
+					acc[key] = error.message;
+					return acc;
+				}, {});
+				setErrors(formattedErrors);
+				return false;
+			}
+			return true;
+		};
 
-              <div className="mb-4 md:flex md:justify-between gap-3">
-                <div className="w-full md:w-1/2">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="tempatLahir"
-                  >
-                    Tempat Lahir
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="tempatLahir"
-                    type="text"
-                    onChange={(e) =>
-                      setData({ ...data, tempatLahir: e.target.value })
-                    }
-                  />
-                </div>
-                <div className="w-full md:w-1/2">
-                  <label
-                    htmlFor="jenisKl"
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                  >
-                    Jenis Kelamin
-                  </label>
-                  <select
-                    id="jenisKl"
-                    defaultValue={data["jenisKelamin"]}
-                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    onChange={(e) =>
-                      setData({ ...data, jenisKelamin: e.target.value })
-                    }
-                  >
-                    <option value="" disabled>
-                      Pilih
-                    </option>
-                    <option value="P">Perempuan</option>
-                    <option value="L">Laki Laki</option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex gap-4">
-                <button
-                  className="btn btn-secondary"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Cancel
-                </button>
-                <button className="btn btn-primary" type="submit">
-                  Submit
-                </button>
-              </div>
-            </form>
-          </DialogPanel>
-        </div>
-      </Dialog>
-    </>
-  );
+		if (!validate(dptNew)) {
+			Swal.fire({
+				title: "Gagal",
+				icon: "error",
+			});
+			return;
+		}
+		postDpt({ data: dptNew })
+			.then((res) => {
+				if (res.status < 300) {
+					onSucces(res.data["data"]);
+					setIsOpen(false);
+				}
+			})
+			.catch(({ response }) => {});
+	};
+
+	return (
+		<>
+			<button
+				type="button"
+				className="btn btn-outline btn-primary flex whitespace-nowrap"
+				onClick={() => setIsOpen(!isOpen)}
+			>
+				+ DPT
+			</button>
+			<Dialog
+				open={isOpen}
+				onClose={() => setIsOpen(false)}
+				className="relative z-50"
+			>
+				<div className="fixed inset-0 flex w-screen items-center justify-center p-4 bg-gray-900 bg-opacity-40 backdrop-blur">
+					<DialogPanel className="max-w-xl space-y-4 border rounded bg-white p-4 shadow">
+						<DialogTitle className="font-bold">Tambah DPT</DialogTitle>
+						<Description>
+							Tambahkan data DPT jika tidak ada data yang sesuai.
+						</Description>
+						<form className="space-y-3" onSubmit={onSubmit}>
+							<div className="flex gap-3 flex-col md:flex-row">
+								<div className="w-full">
+									<label>Pilih Kecamatan</label>
+									<KecamatanSelect2024
+										kabId={kabKode}
+										onChange={(kec) => {
+											setKecamatan(kec);
+											setKelurahan(null);
+										}}
+										kecamatan={kecamatan}
+									/>
+								</div>
+							</div>
+							<div className="flex gap-3 mt-3 flex-col md:flex-row">
+								<div className="w-full md:w-1/2">
+									<label>Pilih Kelurahan</label>
+									<KelurahanSelect2024
+										kecId={kecamatan?.wilId}
+										kelurahan={kelurahan}
+										kabId={kabKode}
+										onChange={(kel) => {
+											setKelurahan(kel);
+										}}
+									/>
+								</div>
+								<div className="w-full md:w-1/2">
+									<label htmlFor="namaTps">Pilih TPS</label>
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="namaTps"
+										type="text"
+										onChange={(e) =>
+											setData({ ...data, namaTps: e.target.value })
+										}
+									/>
+								</div>
+							</div>
+							<div className="mb-4 md:flex md:justify-between gap-3">
+								<div className="w-full md:w-1/2">
+									<label
+										className="block mb-2 text-sm font-bold text-gray-700"
+										htmlFor="name"
+									>
+										Nama
+									</label>
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="name"
+										type="text"
+										placeholder="Nama"
+										onChange={(e) => setData({ ...data, nama: e.target.value })}
+									/>
+								</div>
+								<div className="w-full md:w-1/2">
+									<label
+										className="block mb-2 text-sm font-bold text-gray-700"
+										htmlFor="nik"
+									>
+										NIK
+									</label>
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="nik"
+										type="text"
+										placeholder="73xxxx"
+										max={16}
+										onChange={(e) => setData({ ...data, nik: e.target.value })}
+									/>
+								</div>
+							</div>
+							<div className="mb-4 md:flex md:justify-between gap-3">
+								<div className="w-full md:w-1/2">
+									<label
+										className="block mb-2 text-sm font-bold text-gray-700"
+										htmlFor="rt"
+									>
+										RT
+									</label>
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="rt"
+										type="text"
+										placeholder="001"
+										onChange={(e) => setData({ ...data, rt: e.target.value })}
+									/>
+								</div>
+								<div className="w-full md:w-1/2">
+									<label
+										className="block mb-2 text-sm font-bold text-gray-700"
+										htmlFor="rw"
+									>
+										RW
+									</label>
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="rw"
+										type="text"
+										placeholder="00"
+										max={16}
+										onChange={(e) => setData({ ...data, rw: e.target.value })}
+									/>
+								</div>
+							</div>
+
+							<div className="mb-4 md:flex md:justify-between gap-3">
+								<div className="w-full md:w-1/2">
+									<label
+										className="block mb-2 text-sm font-bold text-gray-700"
+										htmlFor="tempatLahir"
+									>
+										Tempat Lahir
+									</label>
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="tempatLahir"
+										type="text"
+										onChange={(e) =>
+											setData({ ...data, tempatLahir: e.target.value })
+										}
+									/>
+								</div>
+								<div className="w-full md:w-1/2">
+									<label
+										htmlFor="jenisKl"
+										className="block mb-2 text-sm font-bold text-gray-700"
+									>
+										Jenis Kelamin
+									</label>
+									<select
+										id="jenisKl"
+										defaultValue={data["jenisKelamin"]}
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										onChange={(e) =>
+											setData({ ...data, jenisKelamin: e.target.value })
+										}
+									>
+										<option value="" disabled>
+											Pilih
+										</option>
+										<option value="P">Perempuan</option>
+										<option value="L">Laki Laki</option>
+									</select>
+									<InputErrorMessage error={errors["jenisKelamin"]} />
+								</div>
+							</div>
+							<div className="flex gap-4">
+								<button
+									className="btn btn-secondary"
+									onClick={() => setIsOpen(false)}
+								>
+									Cancel
+								</button>
+								<button className="btn btn-primary" type="submit">
+									Submit
+								</button>
+							</div>
+						</form>
+					</DialogPanel>
+				</div>
+			</Dialog>
+		</>
+	);
 }
--- app/(dashboard)/program-harapan/pageClient.tsx
+++ app/(dashboard)/program-harapan/pageClient.tsx
@@ -69,6 +69,12 @@ export default function ProgramRelawanClient({ filters }: Props) {
 			ssr: false,
 		},
 	);
+	useEffect(() => {
+		if (search.length > 0) {
+			setPage(1);
+		}
+		return () => {};
+	}, [search]);
 	if (!user?.role) return "";
 
 	const FilterData = (
@@ -218,7 +224,6 @@ export default function ProgramRelawanClient({ filters }: Props) {
 							value={search}
 							onChange={(e) => {
 								setSearch(e.target.value);
-								setPage(1);
 							}}
 							className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
 						/>
